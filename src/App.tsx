5dca9a040a09060f34c229bdc2c945164f054340
Thomas Haller <Thomas.Haller@lab10.coop>
2021-08-14 23:16:36 +0200

    display number of current validators.

--- src/App.tsx
+++ src/App.tsx
@@ -79,7 +79,7 @@ class App extends React.Component<AppProps, {}> {
           <p>
             account: <span className="text-primary">{context.myAddr}</span> |
             balance: {context.myBalance.print()} {context.coinSymbol}<br />
-            current block nr: {context.currentBlockNumber} | current epoch: {context.stakingEpoch} | epoch start Block {context.epochStartBlock} | epoch start Time {new Date(context.epochStartTime * 1000).toLocaleString()} | deltaPot {context.deltaPot} | reinsertPot {context.reinsertPot}
+            current block nr: {context.currentBlockNumber} | current epoch: {context.stakingEpoch} | epoch start Block {context.epochStartBlock} | epoch start Time {new Date(context.epochStartTime * 1000).toLocaleString()} | deltaPot {context.deltaPot} | reinsertPot {context.reinsertPot} | validators# | {context.pools.filter(x=>x.isCurrentValidator).length})
             {/* <span className={`${this.isStakingAllowed ? 'text-success' : 'text-danger'}`}> staking {this.stakingAllowedState}: {context.stakingAllowedTimeframe} blocks</span> */}
             {validatorsWithoutPoolSection}
           </p>
